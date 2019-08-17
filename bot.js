082ffb465553cddbe1f894e35d6d0db9cb2d393b
Raedaaa <49628200+Raedaaa@users.noreply.github.com>
2019-08-17 22:40:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "+";
+var prefix = "M";
 var adminprefix = '#'
 
 
-const developers = ["ID","ID"]
+const developers = ["529263972172693504","529263972172693504"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
